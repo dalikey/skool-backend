2dcfa0c79ed5ba5936b02ec027142578fa44f363
Geoffrey Westhoff <geoffreywesthoff@gmail.com>
2022-05-31 18:47:10 +0200

    Refactor misspelled function name & add example.env file

--- src/routes/registration.routes.ts
+++ src/routes/registration.routes.ts
@@ -4,7 +4,7 @@ const registrationRouter  = express.Router();
 
 registrationRouter.post('/api/auth/register',
 registrationController.verifyInput,
-registrationController.hashPashword,
+registrationController.hashPassword,
 registrationController.registerUser
 );
 
