6640971ab989298e4bc9ee1153d86d4dd16eb2be
xin <xin20wang@outlook.com>
2022-06-09 19:10:35 +0200

    Prevents duplicate enrollments to the same shift

--- src/controller/enrollment.controller.ts
+++ src/controller/enrollment.controller.ts
@@ -36,12 +36,29 @@ const controller = {
             res.status(401).json({error: "limit_reached", message: "maximum amount of participants reached."});
         }
     },
+    async checkEnrollmentExistence(req:any, res:any, next:any){
+        const userId = res.locals.decodedToken;
+        const shiftId = req.params.shiftId;
+        const result = await queryCommands.checkEnrollmentOfUser(shiftId, userId);
+        if(result){
+            res.status(400).json({error: "already_enrolled", message: "user has already been enrolled"});
+        } else{
+            next();
+        }
+    }
+    ,
     async enrollToShift(req: any, res: any){
         const userId = res.locals.decodedToken;
         const WshiftId = req.params.shiftId;
         const enrollmentObject = { userId: new ObjectId(userId), shiftId: new ObjectId(WshiftId), enrollDate: DateTime.now(), status: "Pending", motivation: req.body.motivation};
-        const enroll = await queryCommands.enrollToShift(WshiftId, enrollmentObject);
-        res.status(201).json({message: "user has send enrollment."})
+        try {
+            const enroll = await queryCommands.enrollToShift(WshiftId, enrollmentObject);
+            res.status(201).json({message: "user has send enrollment."})
+        }catch (e){
+            res.status(401).json({message: "Failure enrollment"});
+        }
+
+
     }
 }
 
