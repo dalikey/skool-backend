19fdfbdfa4e050c0b93088a007c1fa01b7bc139f
sanneve <s.vaneck2@student.avans.nl>
2022-06-09 11:56:01 +0200

    deleted workshopbody element -- wasnt used and wrong info

--- src/models/workshopBody.ts
+++ src/models/workshopBody.ts
@@ -1,16 +1,3 @@
-export interface workshopBody {
-    _id: string;
-    name: string;
-    city: string;
-    street: string;
-    description: string;
-    // date: Date;
-    maxParticipants: number;
-    imageUrl: string;
-    userId: string;
-    isActive: boolean | null;
-}
-
 export interface workshopInsert {
     name: string;
     content: string;
