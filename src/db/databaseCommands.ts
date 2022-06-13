6b271a656d3222e1409aca029dd64e7ca5db617e
xin <xin20wang@outlook.com>
2022-06-13 22:48:39 +0200

    Revert lookup method

--- src/db/databaseCommands.ts
+++ src/db/databaseCommands.ts
@@ -77,23 +77,23 @@ export const queryCommands = {
     async getUser(id: ObjectId) {
         const projection = {password: 0};
         Logger.info(projection);
-        const aggr = [
-            {
-            '$lookup':{
-                'from': 'workshop',
-                'localField': 'workshopPreferences',
-                'foreignField': '_id',
-                'as': 'workshopPreferences'
-            }
-            }, {
-                $project:projection
-            }]
+        // const aggr = [
+        //     {
+        //     '$lookup':{
+        //         'from': 'workshop',
+        //         'localField': 'workshopPreferences',
+        //         'foreignField': '_id',
+        //         'as': 'workshopPreferences'
+        //     }
+        //     }, {
+        //         $project:projection
+        //     }]
         try {
             const collection = await this.getUserCollection();
-            const newQueryResult =await collection.aggregate(aggr).toArray();
-            //const queryResult =  await collection.findOne({_id: id}, {projection});
-            Logger.info(newQueryResult);
-            return newQueryResult;
+            //const newQueryResult =await collection.aggregate(aggr).toArray();
+            const queryResult =  await collection.findOne({_id: id}, {projection});
+            Logger.info(queryResult);
+            return queryResult;
         } catch (err: any) {
             return {status: 500, error: err.message}
         }
