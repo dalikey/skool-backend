c0ea29162184fe6e93a77f066f56c1c5edefd893
xin <xin20wang@outlook.com>
2022-06-13 09:00:04 +0200

    Minor changes

--- src/controller/enrollment.controller.ts
+++ src/controller/enrollment.controller.ts
@@ -195,7 +195,6 @@ const controller = {
            assert(typeof unknownUser.lastName == 'string');
            assert(typeof unknownUser.emailAddress == 'string');
            assert(typeof unknownUser.phoneNumber == 'string');
-           assert(typeof unknownUser.bankNumber == 'string');
         } catch (e) {
             return res.status(400).json({error: "input_error", message: "Missing inputfields"});
         }
